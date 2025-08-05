47f6f32b7e97e70c08758a9424380d7e3ed4a204
Pedro Pimentel <pedropimetel900@gmail.com>
2025-08-05 00:33:44 -0300

    feat: adiciona player de música e arquivos de áudio ao projeto

---
+++ BACK/controllers/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const perfil = document.querySelector(".spanOne");
+
+  if (perfil) {
+    perfil.addEventListener("click", () => {
+      window.location.href = "../perfil/pgPerfil.html";
+    });
+  }
+});
